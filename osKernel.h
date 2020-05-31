e15dc4e2196d01869ec41a09598243f5219b6751
jdvm05 <60103440+jdvm05@users.noreply.github.com>
2020-05-31 12:25:12 -0500

    Add files via upload

---
+++ osKernel.h
@@ -0,0 +1,16 @@
+#ifndef _OS_KERNEL_H
+#define _OS_KERNEL_H
+#include "TM4C123.h"                    // Device header
+
+#include <stdint.h>
+void osKernelLaunch(uint32_t quanta);
+
+void osKernelInit(void);
+
+uint8_t osKernelAddThreads(void(*task0)(void),
+													 void(*task1)(void),
+													 void(*task2)(void));
+
+void osThreadYield(void);
+													 
+#endif
